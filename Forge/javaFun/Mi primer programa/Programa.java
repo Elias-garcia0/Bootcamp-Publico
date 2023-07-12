0e1a5790359ac5e5f85704b2a33c529d1b45cdfb
Elias Garcia <129087002+Elias-garcia0@users.noreply.github.com>
2023-07-12 00:14:48 -0300

    Add files via upload

---
+++ Forge/javaFun/Mi primer programa/Programa.java
@@ -0,0 +1,8 @@
+public class Programa{
+    public static void main(String[] args) {
+        System.out.println("Mi nombre es Elias");
+        System.out.println("Tengo 19 años de edad");
+        System.out.println("Mi ciudad es Montevideo");
+    }
+    
+}
