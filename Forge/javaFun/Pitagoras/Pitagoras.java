5ddf5a3355fa7d19426b6b04bd08a551939c0f1c
Elias Garcia <129087002+Elias-garcia0@users.noreply.github.com>
2023-07-13 22:04:24 -0300

    Add files via upload

---
+++ Forge/javaFun/Pitagoras/Pitagoras.java
@@ -0,0 +1,8 @@
+public class Pitagoras {
+    public static void main(String[] args) {
+        Teorema calculador = new Teorema();
+
+        double resultado = calculador.calcularHipotenusa(6, 5);
+        System.out.println("El resultado es: " + resultado);
+    }
+}
---
+++ Forge/javaFun/Pitagoras/Teorema.java
@@ -0,0 +1,9 @@
+public class Teorema {
+    public double calcularHipotenusa(double catetoA, double catetoB) {
+        double resultado = 0.0;
+        if (Math.abs(catetoA) > Math.abs(catetoB)) {
+            resultado = Math.sqrt((catetoA * catetoA) + (catetoB * catetoB));
+        }
+        return resultado;
+    }
+}
