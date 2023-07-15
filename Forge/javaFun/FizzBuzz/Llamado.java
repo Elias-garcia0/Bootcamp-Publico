6a969f499392ce9b44d5075462b92b7d89304929
Elias Garcia <129087002+Elias-garcia0@users.noreply.github.com>
2023-07-15 01:39:22 -0300

    Add files via upload

---
+++ Forge/javaFun/FizzBuzz/Llamado.java
@@ -0,0 +1,8 @@
+public class Llamado {
+    public static void main(String[] args) {
+        FizzBuzz fb = new FizzBuzz();
+        System.out.println(fb.fizzBuzz(6));
+        System.out.println(fb.fizzBuzz(15));
+         System.out.println(fb.fizzBuzz(10));
+    }
+}
